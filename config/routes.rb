4423f2274f1314f3e386da362e6c1a19622f64aa
Kaiying Fu <kaiying.fu@gmail.com>
2021-05-28 14:07:41 +0800

    First commit to gh-pages branch

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'books#index'
-
-  resources :books, only: [:index, :create]
-  resources :books, only: [:show] do
-    get :reader
-  end
-end
--- db/migrate/20210527120841_add_text_to_books.rb
+++
@@ -1,5 +0,0 @@
-class AddTextToBooks < ActiveRecord::Migration[6.0]
-  def change
-    add_column :books, :text, :string, array: true
-  end
-end
